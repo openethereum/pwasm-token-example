36f53b5bbc10b0e0b4562844b1f9db9062ede07f
NikVolf <nikvolf@gmail.com>
2018-01-15 16:00:22 +0300

    finalize wasm32-unknown-unknwon transform

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cargo build --release --target wasm32-unknown-unknown
-wasm-build ./target pwasm-token-contract-bin --target=wasm32-unknown-unknown --final=token --save-raw=./target/token-raw.wasm
+wasm-build ./target token --target=wasm32-unknown-unknown --final=token --save-raw=./target/token-deployed.wasm
 
 cp ./target/*.wasm ./compiled
 cp ./target/json/* ./compiled
