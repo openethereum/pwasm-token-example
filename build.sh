559e5c724b297f9d0b66726bd9c5e04821f9fe03
NikVolf <nikvolf@gmail.com>
2018-01-15 16:59:00 +0300

    z-level and cool opt

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cargo build --release --target wasm32-unknown-unknown
-wasm-build ./target token --target=wasm32-unknown-unknown --final=token --save-raw=./target/token-deployed.wasm
+wasm-build ./target pwasm_token_contract_bin --target=wasm32-unknown-unknown --final=token --save-raw=./target/token-deployed.wasm
 
 cp ./target/*.wasm ./compiled
 cp ./target/json/* ./compiled
