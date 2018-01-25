d713ca9ef41459e6109ebcde72069d18975521fc
fro <frol.rage@gmail.com>
2018-01-25 20:45:56 +0300

    small fix

--- src/token.rs
+++ src/token.rs
@@ -13,11 +13,11 @@ use pwasm_abi::eth::EndpointInterface;
 #[no_mangle]
 pub fn call(desc: *mut u8) {
     // pwasm_std::parse_args parses the call descriptor into arguments and result pointers
+    let (args, result) = unsafe { pwasm_std::parse_args(desc) };
+    let mut endpoint = pwasm_token_contract::Endpoint::new(pwasm_token_contract::TokenContractInstance{});
     // Args is an Solidity-compatible abi call: first 4 bytes are the Method ID of keccak hash of function signature
     // followed by sequence of arguments packed into chunks of 32 bytes.
     // Read http://solidity.readthedocs.io/en/develop/abi-spec.html#formal-specification-of-the-encoding for details
-    let (args, result) = unsafe { pwasm_std::parse_args(desc) };
-    let mut endpoint = pwasm_token_contract::Endpoint::new(pwasm_token_contract::TokenContractInstance{});
     result.done(endpoint.dispatch(&args));
 }
 
