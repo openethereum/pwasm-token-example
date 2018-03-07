b0df0b9f756208a38ee91b17d1c45050ba87c96a
Alexey <frol.rage@gmail.com>
2018-03-07 12:08:40 +0300

    Update lib.rs
    comments update and fix wording

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -25,15 +25,15 @@ use pwasm_abi_derive::eth_abi;
 // TokenContract is an interface definition of a contract.
 // The current example covers the minimal subset of ERC20 token standard.
 // eth_abi macro parses an interface (trait) definition of a contact and generates
-// two structs: Endpoint and Client.
+// two structs: `Endpoint` and `Client`.
 //
-// Endpoint is an entry point for contract calls.
+// The `Endpoint` is an entry point for contract calls.
 // eth_abi macro generates a table of Method IDs corresponding with every method signature defined in the trait
 // and defines it statically in the generated code.
-// Scroll down at "pub fn call(desc: *mut u8)" to see how
-// Endpoint instantiates with a struct TokenContractInstance which implements the trait definition.
+// See https://github.com/paritytech/pwasm-token-example/blob/master/src/token.rs how generated `Endpoint` is used in `call` and `deploy` (constructor).
+// `Endpoint` instantiates with a struct TokenContractInstance which implements the trait definition.
 //
-// Client is a struct which is useful for call generation to a deployed contract. For example:
+// The `Client` is a struct which is useful for call generation to a deployed contract. For example:
 // ```
 //     let mut client = Client::new(contactAddress);
 //     let balance = client
